707f50ad5d79e21f0dfa49de34c97b8c634ab75b
gong-b <917847939@qq.com>
2026-04-04 10:32:50 +0800

    Update excel_handler.py

--- excel_handler.py
+++ excel_handler.py
@@ -1,19 +1,21 @@
 import pandas as pd
-from typing import Any
-from config import ADMITTED_FILE, REJECTED_FILE
+import config
 
-def read_student_list(file_path: str) -> set[str]:
-    """修复学号列匹配问题"""
-    try:
-        # 动态匹配包含"学号"的列名
-        df: pd.DataFrame = pd.read_excel(file_path, sheet_name="sheet1")# type:ignore
-        id_col: list[str] = [col for col in df.columns.tolist() if "学号" in col]
+class ExcelHandler:
+    def read_student_ids(self, file_path):
+        try:
+            df = pd.read_excel(file_path)
+            for col in df.columns:
+                if "学号" in str(col):
+                    return [str(x).strip() for x in df[col] if pd.notna(x)]
+        except:
+            return []
+        return []
 
-        return set(df[id_col[0]].astype(str).tolist())
-    except Exception as e:
-        print(f"读取失败: {file_path} - 错误信息: {e}")
-        return set()
-def save_results(admitted: list[Any], rejected: list[Any]):
-    """保存录取结果"""
-    pd.DataFrame(admitted).to_excel(ADMITTED_FILE, sheet_name="Sheet1", index=False) # type: ignore
-    pd.DataFrame(rejected).to_excel(REJECTED_FILE, sheet_name="Sheet1", index=False) # type: ignore
+    def write_accept(self, data):
+        df = pd.DataFrame(data, columns=["学号", "姓名", "状态"])
+        df.to_excel(config.ADMITTED_FILE, index=False)
+
+    def write_reject(self, data):
+        df = pd.DataFrame(data, columns=["学号", "姓名", "拒绝原因"])
+        df.to_excel(config.REJECTED_FILE, index=False)
