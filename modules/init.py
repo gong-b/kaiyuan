ebc9f6c63c1489746c0316e9f94a3db45c2d6052
gong-b <917847939@qq.com>
2026-04-20 19:00:48 +0800

    Create init.py

---
+++ modules/init.py
@@ -0,0 +1,5 @@
+from .config import Config
+from .email_parser import EmailParser
+from .email_client import SecureIMAPClient
+from .excel_handler import ExcelHandler
+from .docx_parser import DocxParser
