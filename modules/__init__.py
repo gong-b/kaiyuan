2c6924e569bf0c94e40fc45488f76b86c454ddf6
gong-b <917847939@qq.com>
2026-04-21 19:50:02 +0800

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -2,4 +2,4 @@ from .config import Config
 from .email_parser import EmailParser
 from .email_client import SecureIMAPClient
 from .excel_handler import ExcelHandler
-from .docx_parser import DocxParser
+from .file_parser import FileParser
